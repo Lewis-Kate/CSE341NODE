4ee1d3549b91583622159b3a5dd7f37d7e862330
Lewis-Kate <35283629+Lewis-Kate@users.noreply.github.com>
2020-11-13 23:54:44 -0500

    generic

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+.get('/', myCallbackFunction)
 
+function myCallbackFunction() {
+  res.render('pages/index')
+}
 
 app.get('/results', calculateRate)
 
