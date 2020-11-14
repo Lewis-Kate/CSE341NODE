e0cb4924816d5bdc9069440e801f8b7756da55bd
Lewis-Kate <35283629+Lewis-Kate@users.noreply.github.com>
2020-11-13 23:46:37 -0500

    callback

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-function calculateRate() {
+calculateRate {
   res.render('pages/index')
 }
 
