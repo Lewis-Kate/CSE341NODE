0ccc57bf8faa5a6bfb33e4a7a614da59e396007a
Lewis-Kate <35283629+Lewis-Kate@users.noreply.github.com>
2020-11-14 21:01:45 -0500

    /

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const calculateRate = require('./lib/calculateRate');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/'));
 
-app.use('/public', express.static(path.join(__dirname, "../public")));
+app.use('/public/', express.static(path.join(__dirname, "../public/")));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
