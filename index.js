1f51b3b0fa83c5cd9d6ece1b5001b73c6c04df25
Lewis-Kate <35283629+Lewis-Kate@users.noreply.github.com>
2020-11-14 20:54:39 -0500

    path

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const app = express();
 
@@ -7,6 +8,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+app.use('/public', express.static(path.join(__dirname, "../public")));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -19,4 +21,5 @@ app.get('/results', calculateRate)
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
 
