349f6786d4c7e053cc16fae2b13e79182ff31968
Lewis-Kate <35283629+Lewis-Kate@users.noreply.github.com>
2020-11-21 12:45:00 -0500

    movies app

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express');
 const app = express();
 
-const calculateRate = require('./lib/calculateRate');
+const movies = require('./lib/movies');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,9 +12,9 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 
-app.get('/', (req, res) => res.render('pages/form'))
+app.get('/', (req, res) => res.render('pages/index'))
 
-app.get('/results', calculateRate)
+//app.get('/results', calculateRate)
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
