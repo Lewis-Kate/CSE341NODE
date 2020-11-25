4bd3d8d97c13548f03d7c740f2ff8cb4370f72dd
Lewis-Kate <35283629+Lewis-Kate@users.noreply.github.com>
2020-11-24 22:44:10 -0500

    path

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public/'));
 
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
