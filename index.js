1e27fa7dfe7a39f64d06b2bd7be2b719c07d657f
Lewis-Kate <35283629+Lewis-Kate@users.noreply.github.com>
2020-11-24 23:02:36 -0500

    index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express');
 const app = express();
 
-const movies = require('./lib/movies');
+const movies = require('./js/movies');
 
 app.set('port', (process.env.PORT || 5000));
 
