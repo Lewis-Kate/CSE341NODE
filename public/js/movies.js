38f6fa6bd98caedc3d1e90624ebe9cebd832043c
Lewis-Kate <35283629+Lewis-Kate@users.noreply.github.com>
2020-12-08 21:39:53 -0500

    plot

--- public/js/movies.js
+++ public/js/movies.js
@@ -39,5 +39,42 @@ function updateResults(data) {
 			resultList.empty();
 			resultList.append("<p class='title warning'>Please try again</p>");
 		}
+    
+    function findPlot() {
+	// Get the value from the search box
+	let searchTitle = $("#title").val();
+	console.log("Searching for: " + searchTitle);
+
+	// Set up the parameters to send to the API
+	let params = {t: searchTitle, apikey:"c214de2e"};
+
+	// Use jQuery to make the get request
+	$.get("https://www.omdbapi.com/", params, function(data, status){
+		// For debugging purposes, make a note that we're back
+		console.log("Plot from API:")
+		console.log(status);
+    	console.log(data);
+
+    	updateResults(data)
+	});
+}
+    
+    function updatePlot(data) {
+
+	if (data.Search && data.Search.length > 0) {
+		var resultList = $("#dataResults");
+		resultList.empty();
+
+		for (let i = 0; i < data.Search.length; i++) {
+			let plot = data.Search[i].Plot;
+            
+            resultList.append("<div class='plot'><p class='titleText'>" + plot + "</p></div>");
+		} 
+	} else {
+			let resultList = $("#dataResults");
+			resultList.empty();
+			resultList.append("<p class='title warning'>Please try again</p>");
+		}
+			
 
 }
